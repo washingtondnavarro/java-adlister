ed049b53228a7c51b86c6f0cb6e910aac3b55e03
washingtondnavarro <washington.d.navarro@gmail.com>
2017-12-18 16:47:39 -0600

    finised the exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,19 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "Hello World" , urlPatterns = "/hello")
 
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+        String name = req.getParameter("name");
+        res.getWriter().println("Hello, " + name + "!");
+
+
 
     }
 }
--- src/main/java/ViewCount.java
+++ src/main/java/ViewCount.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class ViewCount {
+
+
+@WebServlet(name = "ViewCount" , urlPatterns = "/counter")
+
+public class ViewCount extends HttpServlet{
+    int counter = 0;
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        counter += 1;
+        String name = req.getParameter("name");
+        res.getWriter().println("<h1> Hello, " + name + "! </h1>");
+        res.getWriter().println("<h1> Pages visited " + counter + "times </h1>");
+
+
+
+    }
 }
